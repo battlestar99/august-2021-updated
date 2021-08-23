bd187bc4725495398273e821606f25bb85686823
“[Razin <anik@agile1tech.com>
2021-08-23 19:45:58 -0400

    selenium library added

---
+++ programmingMethod/src/package1/car.java
@@ -0,0 +1,29 @@
+package package1;
+
+public class car {
+	
+	
+	//methods
+	//attributes 
+	
+	
+	public static void samplefunction() {
+		
+		System.out.println("this is a sample function");
+	}
+	
+	
+	
+	public void start() {
+		
+		System.out.println("car is starting");
+		
+		
+	}
+	
+   public void accelate () {
+		
+		System.out.println("car is accelating");
+	}
+
+}
---
+++ programmingMethod/src/package1/mainRun.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class mainRun {
+
+	public static void main(String[] args) {
+		
+		car.samplefunction();
+		
+		car c = new car();
+		
+		c.accelate();
+		
+		car d = new car();
+		
+		d.start();
+		
+		
+		
+		
+
+	}
+
+}
